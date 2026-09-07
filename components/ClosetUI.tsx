76a69ddded59be49c2e8554d3dce81a224000e55
KshaunishSoni <kshaunishsoni123@gmail.com>
2026-09-07 15:40:25 -0500

    removing slot head pt 2

--- components/ClosetUI.tsx
+++ components/ClosetUI.tsx
@@ -46,7 +46,6 @@ export default function ClosetUI({ clothes }: { clothes: any[] }) {
               </div>
             );
           })}
-          <div id="slot-head" className="slot"></div>
         </div>
       </div>
       
